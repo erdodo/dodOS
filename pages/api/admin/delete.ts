8a7b8dace3345f0e8b964654aab9a931e3d22f47
erdodo <erdoganyesil3@gmail.com>
2023-09-28 00:01:44 +0300

    feat(DOS-12): Silme işlemleri

---
+++ pages/api/admin/delete.ts
@@ -0,0 +1,20 @@
+import {get_data, set_data} from '../db'
+export default async function handler (req:any, res:any) {
+    const id = req.query.id;
+    let datas ={}
+    await get_data(req.query.collection).then(async(data)=>{
+        datas = data
+        datas= {
+            ...req.body,
+            isDeleted:true
+        }
+        await set_data(req.query.collection,req.query.id,datas).then((data:any)=>{
+            res.status(200).json(data)
+        }).catch(err=>{
+            res.status(400).json({error:err, message:"Error getting data"})
+        })
+    })
+
+}
+
+
