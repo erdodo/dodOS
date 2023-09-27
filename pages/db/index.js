ea4f93c05709cfc5e2f8f5b446d5ce742765a38c
erdodo <erdoganyesil3@gmail.com>
2023-09-27 12:27:56 +0300

    feat(DOS-35): Firebase Bağlantısı
    Firebase bağlantısı yapıldı. Listeleme isteği başarıyla gönderildi.

---
+++ pages/db/index.js
@@ -0,0 +1,31 @@
+import { initializeApp, applicationDefault, cert,getApps } from 'firebase-admin/app';
+import { getFirestore, Timestamp, FieldValue, Filter } from 'firebase-admin/firestore';
+import firebase from "firebase/app"
+
+const firebaseConfig = {
+    "type": "service_account",
+    "project_id": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.NEXT_PUBLIC_FIREBASE_AUTH_URI,
+    "token_uri": process.env.NEXT_PUBLIC_FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.NEXT_PUBLIC_FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.NEXT_PUBLIC_FIREBASE_CLIENT_X509_CERT_URL,
+    "universe_domain": "googleapis.com"
+}
+
+
+getApps().length===0 ? initializeApp({
+    credential: cert(firebaseConfig)
+}) : null;
+
+
+
+export const get_data = async(collection,document)=>{
+    const db = getFirestore();
+    const docRef = db.collection(collection).doc(document);
+
+    return await docRef.get()
+}
