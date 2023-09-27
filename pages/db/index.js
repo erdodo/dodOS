65e17cfb4efed4ceec722ada7fe625fef49837c0
erdodo <erdoganyesil3@gmail.com>
2023-09-27 12:29:42 +0300

    feat(DOS-35): Gereksizler kaldırıldı

--- pages/db/index.js
+++ pages/db/index.js
@@ -1,6 +1,5 @@
-import { initializeApp, applicationDefault, cert,getApps } from 'firebase-admin/app';
-import { getFirestore, Timestamp, FieldValue, Filter } from 'firebase-admin/firestore';
-import firebase from "firebase/app"
+import { initializeApp, cert,getApps } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 
 const firebaseConfig = {
     "type": "service_account",
